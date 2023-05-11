ddc74a2efd63c89cb55a1474d543bce6c462ea32
Ernesto Campos <ernesto.campos@utp.ac.pa>
2023-05-10 23:28:52 -0500

    p2

---
+++ Campos-Ernesto/Lab3-ErnestoCampos/main.js
@@ -0,0 +1,17 @@
+(()=>{
+    const LAB3 = {
+        htmlElements: {
+            PalindromoForm: document.getElementById('PalindromoForm'),
+        },
+        init(){
+            LAB3.htmlElements.PalindromoForm.addEventListener('submit',);
+        },
+        handlers: {
+            
+        },
+        methods: {
+            
+        },
+    };
+    LAB3.init();
+})();
