70f24065434ec8a7c571b4cae71fa85425d25f2f
Ernesto Campos <ernesto.campos@utp.ac.pa>
2023-05-13 21:05:59 -0500

    Lab 4 - Añadí comentarios para rehuso

--- Campos-Ernesto/Lab4-ErnestoCampos/main.js
+++ Campos-Ernesto/Lab4-ErnestoCampos/main.js
@@ -1,3 +1,6 @@
+//Ernesto Campos, 8-977-1599
+//Con las anotaciones que estoy colocando en este JS, puedo volver a entender el codigo despues de un tiempo.
+
 (()=>{
     const LAB4 = {
         htmlElements: {
@@ -18,27 +21,30 @@
             },
 
             DivClickHandler(event) {
-                if (event.target.tagName === 'DIV') {
-                  const confirmDelete = confirm('¿Deseas borrar este elemento?');
-                  if (confirmDelete) {
-                    event.target.remove();
+                if (event.target.tagName === 'DIV') { //Se verifica que el elemento seleccionado sea un div mediante el tagName
+                  const confirmDelete = confirm('¿Deseas borrar este elemento?'); //Aca se crea una ventana de confirmacion para borrar el elemento que se selecciono
+                  if (confirmDelete) { //Si se acepta la confirmacion, se remueve el elemento
+                    event.target.remove(); //Se remueve el elemento seleccionado
                   }
+                  //No hace falta una parte else ya que si se cancela la confirmacion, no se remueve el elemento
                 }
               },
         },
         methods: {
+
+            //Funcion para obtener la secuencia de fibonacci... Se inicializa con numer1 en 0 y number2 en 1 que son los primeros en la secuencia, y se va sumando el numero anterior con el actual
             getSecuenciaFibonacci(NumberInput) {
                 let SecuenciaFibonacci = [];
                 let Number1 = 0;
                 let Number2 = 1;
                 let Number3 = 0;
                 for (let i = 0; i < NumberInput; i++) {
-                  SecuenciaFibonacci.push(Number1);
+                  SecuenciaFibonacci.push(Number1); //Con esta funcion se agrega el numero a la secuencia
                   Number3 = Number1 + Number2;
-                  Number1 = Number2;
-                  Number2 = Number3;
+                  Number1 = Number2; //Se cambia el valor de number1 por el de number2
+                  Number2 = Number3; //Se cambia el valor de number2 por el de number3
                 }
-                //console.log(SecuenciaFibonacci);
+                
                 return SecuenciaFibonacci;
               },
 
